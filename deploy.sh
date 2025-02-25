bdf66d8a26923df8d4d27896415a67abed7602af
Gustavo Tavares <grstavares@gmail.com>
2025-02-25 16:31:01 +0100

    adjust deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,9 @@ aws s3 cp profile.png s3://www.tavares.cloud --profile power
 aws s3 cp light.jpg s3://www.tavares.cloud --profile power
 aws s3 cp dark.jpg s3://www.tavares.cloud --profile power
 aws cloudfront create-invalidation --distribution-id E3CNISU7P26T4T --paths '/*' --profile power
+
+cp ./index.html ../grstavares.github.io/
+cp ./styles.css ../grstavares.github.io/
+cp ./profile.png ../grstavares.github.io/
+cp ./light.jpg ../grstavares.github.io/
+cp ./dark.jpg ../grstavares.github.io/
